4e1635c805ee2fc45b887c384d5cc142b5813bfa
Zac B <zacbennett0@gmail.com>
2018-12-26 15:10:47 -0800

    took out the debug tools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,8 +13,6 @@ import { Provider } from 'react-redux';
 const store = createStore(
   rootReducer,
   compose(
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk)
   )
 );
